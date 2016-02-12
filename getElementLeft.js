2f021de0b11208f43b3a16cc6fb8b129077a00dd
EVANS <986981086@qq.com>
2016-02-12 16:24:39 +0800

    getElementLeft.js

---
+++ getElementLeft.js
@@ -0,0 +1,13 @@
+function getElementLeft(element){
+
+		var actualLeft=element.offsetLeft;
+		var current=element.offsetParent;
+
+		while(current !== null){
+
+			actualLeft+=current.offsetLeft;
+			current=current.offsetParent;
+		}
+
+		return actualLeft;
+	}
