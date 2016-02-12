a8cb51ba3a118f0c54e4c0b7fa94c42d03559f3d
EVANS <986981086@qq.com>
2016-02-12 16:25:23 +0800

    getElementTop.js

---
+++ getElementTop.js
@@ -0,0 +1,13 @@
+function getElementTop(element){
+
+		var actualTop=element.offsetTop;
+		var current=element.offsetParent;
+
+		while(current !== null){
+
+			actualTop+=current.offsetTop;
+			current=current.offsetParent;
+		}
+
+		return actualTop;
+	}
